2d44a6884e5c7be88bc25ee1e2c4714eaef67f77
Om Patel <omp091220@gmail.com>
2025-12-23 12:22:28 +0530

    Add files via upload

---
+++ Day-01.py
@@ -0,0 +1,5 @@
+#I WILL BE MAKING A BAND NAME GENERATOR USING PYTHON WHICH WILL USE TWO THINGS - THE FIRST IS THE PLACE FROM WHERE YOU BELONG AND THE SECOND IS YOUR FAVORITE ANIMAL
+print("Welcome to the band name generator!")
+birthplace = input("Where are you from?\n")
+animal = input("What is your favorite animal?\n")
+print("\nThe band name can be " + birthplace + " " + animal)
